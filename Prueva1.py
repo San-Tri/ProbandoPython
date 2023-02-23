a9c762d05cd6406dd0a9c338b98ce9e95375cc4b
San-Tri <1473024@senati.pe>
2023-02-23 15:23:33 -0500

    resolviendo en casa lo que no pude en el instituto

---
+++ Prueva1.py
@@ -0,0 +1,6 @@
+# Realizar una suma y una multiplicacion con sus respectivas indicaciones:
+
+# primero poner una variable con su respectivo valor.
+a=23
+b=13
+# ahora tenemos que hacer que los variables pero eso en la siguiente clase...
