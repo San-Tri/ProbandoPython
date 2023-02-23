1f60aeac9839128649f341a76196d3c4a1a79c34
San-Tri <1473024@senati.pe>
2023-02-23 15:34:40 -0500

    resolviendo en casa lo que no pude en el instituto.P3

--- Prueva1.py
+++ Prueva1.py
@@ -6,4 +6,7 @@ b=13
 # usamos print para ver el resultado de la suma.
 print("la suma es:",a+b)
 
-# ahora toca hacer la multiplicacion, pero eso tendra que esperar...
+# para realizar la multiplicacion es la misma sintaxis.
+a=12
+b=12
+print("la respuesta es:",a*b)
